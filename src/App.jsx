a7f8f06eda38bb60c4bda7fbdc6332c96a500598
Kunal Kumar Singh <kunalsg13689@gmail.com>
2024-08-02 02:19:09 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
